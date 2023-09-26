8cc39fd5a45fc8a3dab1a3b0165d6690791a6aed
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-09-27 00:55:54 +0500

    changes

--- Blog Website/script.js
+++ Blog Website/script.js
@@ -2,10 +2,27 @@
 document.addEventListener('DOMContentLoaded', function () {
     const cards = document.querySelectorAll('.card');
 
-    cards.forEach(card => {
-        card.addEventListener('click', () => {
-            // Redirect to the full blog post page or show more content here
-            alert('This is where you would display the full blog post.');
-        });
+// Define an array of URLs corresponding to each card
+const cardUrls = [
+    'https://enchanted-wash.surge.sh',
+    'https://ultra-spring.surge.sh',
+    'https://early-believe.surge.sh/',
+    'https://resolute-pizzas.surge.sh/',
+    'https://auspicious-dark.surge.sh/',
+    'https://telling-dark.surge.sh/',
+    'https://elegant-login.surge.sh/',
+    'https://obese-signin.surge.sh/',
+    'https://black-login.surge.sh/',
+    'https://tearful-signin.surge.sh/',
+    
+    // Add more URLs as needed for each card
+];
+
+// Add a click event listener to each card
+cards.forEach((card, index) => {
+    card.addEventListener('click', () => {
+        // Redirect to the URL associated with the clicked card
+        window.location.href = cardUrls[index];
     });
+})
 });
