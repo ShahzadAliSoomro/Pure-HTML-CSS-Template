d93e158de9ac31de0216814b6e3678717b205bcb
Shahzad Hussain <96440025+ShahzadAliSoomro@users.noreply.github.com>
2023-09-28 01:21:48 +0500

    done

--- Blog Website/script.js
+++ Blog Website/script.js
@@ -1,34 +1,34 @@
 // JavaScript code for handling card click events
-document.addEventListener('DOMContentLoaded', function () {
-    const cards = document.querySelectorAll('.card');
+document.addEventListener("DOMContentLoaded", function () {
+  const cards = document.querySelectorAll(".card");
 
-// Define an array of URLs corresponding to each card
-const cardUrls = [
-    'https://quick-login.surge.sh/',
-    'https://pathetic-signup.surge.sh/',
-    'https://fantastic-login.surge.sh/',
-    'https://illustrious-signup.surge.sh/',
-    'https://elegant-login.surge.sh/',
-    'https://obese-signin.surge.sh/',
-    'https://tame-login.surge.sh/',
-    'https://yalinkrahtihy',
-    'https://zany-login.surge.sh/',
-    'https://strange-signup.surge.sh/',
-    'https://auspicious-dark.surge.sh/',
-    'https://telling-dark.surge.sh/',
-    
-    'https://daffy-login.surge.sh/',
-    'https://true-signup.surge.sh/',
-    
-    
+  // Define an array of URLs corresponding to each card
+  const cardUrls = [
+    "https://quick-login.surge.sh/",
+    "https://pathetic-signup.surge.sh/",
+    "https://fantastic-login.surge.sh/",
+    "https://illustrious-signup.surge.sh/",
+    "https://elegant-login.surge.sh/",
+    "https://obese-signin.surge.sh/",
+    "https://tame-login.surge.sh/",
+    "https://subdued-signup.surge.sh/",
+    "https://zany-login.surge.sh/",
+    "https://strange-signup.surge.sh/",
+    "https://auspicious-dark.surge.sh/",
+    "https://telling-dark.surge.sh/",
+    "https://daffy-login.surge.sh/",
+    "https://true-signup.surge.sh/",
+    "https://likeable-login.surge.sh/",
+    "http://lame-signup.surge.sh/",
+
     // Add more URLs as needed for each card
-];
+  ];
 
-// Add a click event listener to each card
-cards.forEach((card, index) => {
-    card.addEventListener('click', () => {
-        // Redirect to the URL associated with the clicked card
-        window.location.href = cardUrls[index];
+  // Add a click event listener to each card
+  cards.forEach((card, index) => {
+    card.addEventListener("click", () => {
+      // Redirect to the URL associated with the clicked card
+      window.location.href = cardUrls[index];
     });
-})
+  });
 });
